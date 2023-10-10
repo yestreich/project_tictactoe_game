5d1b240ff9ce5bc5efd7c4c0a3796e58de3e8019
yestreich <yannickestreich@web.de>
2023-10-10 16:53:05 +0200

    two player tic tac toe game

--- 48_Aufgabe Tic Tac Toe.py
+++ 48_Aufgabe Tic Tac Toe.py
@@ -5,10 +5,6 @@
 
 #===============#
 
-
-TODO 
-
-Finales Ergbnis wird 2mal gedruckt
 '''
 
 #Zellen-Nummerierung
@@ -53,42 +49,30 @@ def starten():
     print("")
     print(p1 + ", starte das Spiel!")   #Bisher startet Spieler 1 jedes Spiel
     print()
-    # global count                      #klappt irgendwie nicht. Ende wurde anders gelöst-
-    # count = 0
     
     player1()                           
     
 
 def player1():
     
-    # if count > 9:
-    #     ergebnis()
-    # else:
-    #     count += 1
-    
     print(p1 + ", was möchtest du tun?")
     
-    dec = -1                            #damit while-loop öfters laufen kann.
+    dec = -1                            
     while dec < 1 or dec > 2:
         #try:
             print("Spielfeld anzeigen: 1, Stein legen: 2")
             dec = int(input())
             if dec == 1:
                 spielfeld()
-                dec = -1                #damit while-loop aufrechterhalten bleibt. Nach dem steinlegen kommt direkt der nächste Spieler
+                dec = -1                
             if dec == 2:
                 steinlegen("x")
                 
-    if ergebnis() != True:              #Solang kein Gewinner feststeht, darf der nächste Spieler wieder spielen.
+    if ergebnis() != True:              
         player2()
 
 
 def player2():
-    
-    # if count > 9:
-    #     ergebnis()
-    # else:
-    #     count += 1
     
     print(p2 + ", was möchtest du tun?")
     
@@ -101,32 +85,32 @@ def player2():
                 spielfeld()
                 dec = -1
             if dec == 2:
-                steinlegen("o")             #Parameter um Zelle neu zu besetzen      
+                steinlegen("o")                   
     
     if ergebnis() != True:        
         player1()
 
 def steinlegen(s):
-    num = -1                                #While-loop soll wegen möglicher Falscheingabe wiederholt werden können.
+    num = -1                                
     while num < 0:                          
         print()
         print("Du bist " + s + ". Auf welches Feld möchtest du dein Stein legen?")
         print()
         feld = str(input())
-        if feld_list[feld] == feld:         #Wenn die gewünschte Zelle in Dict drin ist, wird sie durch Parameter s ersetzt
+        if feld_list[feld] == feld:         
             feld_list[feld] = s
-            spielfeld()                     #Neues Spielfeld wird angezeigt
-            ergebnis()                      #Nach jedem Zug muss in Funktion berechnet werden ob ein Gewinner feststeht
-            num = 0                         #loop auflösen, da Stein gesetzt wurde
+            spielfeld()                     
+            ergebnis()                      
+            num = 0                        
         else:
             #spielfeld()
             print("Dieses Feld ist belegt. Wähle ein anderes.")
-            continue                        #Falls ein belegtes Feld ausgewählt wird, wird man neu aufgefordert.
+            continue                        
             
 
-def ergebnis():                             #Hier wird das Ergebnis nach jedem Stein neu berechnet
+def ergebnis():                             
     
-    row1 = [feld_list["1"], feld_list["2"], feld_list["3"]] #Listen mit den Reihen-,Spalten-, und Diagonalen-Elementen
+    row1 = [feld_list["1"], feld_list["2"], feld_list["3"]] 
     row2 = [feld_list["4"], feld_list["5"], feld_list["6"]]
     row3 = [feld_list["7"], feld_list["8"], feld_list["9"]]
 
@@ -140,11 +124,11 @@ def ergebnis():                             #Hier wird das Ergebnis nach jedem S
     check = [row1, row2, row3, col1, col2, col3, dia1, dia2]
     
     for i in check:                                 
-        if i.count("x") == 3:                   #Wenn in einem der Reihen, Spalten oder Diagonalen drei X-e sind: Gewonnen.
+        if i.count("x") == 3:                   
             print(p1 + " hat gewonnen.")
             return True
             
-        elif i.count("o") == -3:                #Wenn in einem der Reihen, Spalten oder Diagonalen drei O-s sind: Gewonnen.
+        elif i.count("o") == -3:                
             print(p2 + " hat gewonnen.")
             return True
 
